f70e1bccddca776c9f211b459b2d2bb0ea6e9bd3
tawfiq <tawfiqealahi99@gmail.com>
2024-10-11 12:07:27 +0600

    test

--- practice own/src/App.jsx
+++ practice own/src/App.jsx
@@ -16,10 +16,20 @@ function App() {
 
 function NewC(){
   const age = 30;
+  const objetTest ={
+    name: "razu",
+    age: 20,
+    education: 'PSTU'
+  }
   return(
     <div>
       <h1>my name is tawfiq</h1>
       <p>i am {age} years old.</p>
+      <br />
+      <h2>another</h2>
+      <br />
+    <p>my friend {objetTest.name} is also a student</p>
+    he is {objetTest.age} 
     </div>
   )
 
