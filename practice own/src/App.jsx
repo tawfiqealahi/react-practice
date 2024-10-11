0030577679324318501882f6b463f7477fedd05d
tawfiq <tawfiqealahi99@gmail.com>
2024-10-11 12:01:35 +0600

    react

---
+++ practice own/src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <button>my buttonnpm run dev
+      </button>
+      <br />
+      <NewC></NewC>
+    </div>
+  )
+}
+
+function NewC(){
+  const age = 30;
+  return(
+    <div>
+      <h1>my name is tawfiq</h1>
+      <p>i am {age} years old.</p>
+    </div>
+  )
+
+}
+
+
+
+export default App
