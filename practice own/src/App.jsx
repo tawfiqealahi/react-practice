cc4d55607638fb5bfa92c35f3abb8167c63c4274
tawfiq <tawfiqealahi99@gmail.com>
2024-10-11 20:42:16 +0600

    click me

--- practice own/src/App.jsx
+++ practice own/src/App.jsx
@@ -2,37 +2,15 @@
 import './App.css'
 
 function App() {
- 
+  function handleClick(){
+    alert('BUTTON CLICKED');
+  }
 
   return (
-    <div>
-      <button>my buttonnpm run dev
-      </button>
-      <br />
-      <NewC></NewC>
-    </div>
+   <>
+  <button onClick={handleClick} > click me</button>
+   </>
   )
-}
-
-function NewC(){
-  const age = 30;
-  const objetTest ={
-    name: "razu",
-    age: 20,
-    education: 'PSTU'
-  }
-  return(
-    <div>
-      <h1>my name is tawfiq</h1>
-      <p>i am {age} years old.</p>
-      <br />
-      <h2>another</h2>
-      <br />
-    <p>my friend {objetTest.name} is also a student</p>
-    he is {objetTest.age} 
-    </div>
-  )
-
 }
 
 
